3396d92ff6c8d425b53810c2659e4be569da8d80
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-07-19 16:05:30 +0500

    added number of tasks in column feature

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -5,7 +5,7 @@ import '../styles/Column.css';
 const Column = ({ column, index, filteredTasks, onDragOver, onDrop, onDragStart, onDragEnd, openTaskDetails, openEditForm }) => {
   return (
     <div className="column" onDragOver={onDragOver} onDrop={onDrop}>
-      <h2>{column.title}</h2>
+      <h2>{column.title} ({ column.tasks.length })</h2>
       <div className="tasks">
         {filteredTasks(column.tasks).map((task, taskIndex) => (
           <Task
